7817ab2df0893740e6934a382f4b8d1cd5d0787c
Ranmina Dolawatta <it21101274@my.sliit.lk>
2026-06-24 16:30:26 +0530

    feat:added api endpoints

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Param, Post, Body, Put, Delete, HttpCode, HttpStatus } from '@nestjs/common';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Controller('profiles')
-export class ProfilesController {}
+export class ProfilesController {
+
+  @Get()
+  findAll(@Query('location') location: string) {
+    return [{location}];
+}
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() createProfileDto: CreateProfileDto) {
+    return { 
+      name: createProfileDto.name,
+      description: createProfileDto.description,
+     };
+    
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateProfileDto: UpdateProfileDto) {
+    return { 
+      id,
+      ...updateProfileDto
+     };
+  }
+  @Delete(':id')
+  @HttpCode(HttpStatus.OK)
+  remove(@Param('id') id: string) {}
+
+}
